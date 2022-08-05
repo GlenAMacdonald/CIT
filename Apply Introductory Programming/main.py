f9a05554f9502fe54b6c0b46f20f378d58090750
Spares <macdonald.glen.a@gmail.com>
2022-08-05 13:45:43 +1000

    session2 complete

---
+++ Apply Introductory Programming/main.py
@@ -0,0 +1,13 @@
+
+""" Program will ask the student what their name is
+
+Created by: Glen Macdonald
+Created on: 5-Aug-2022
+Last Edited: 5-Aug-2022
+"""
+
+input_ = input("what is your name?> ")
+print("Hello, " + input_)
+print("Hello,",input_)
+
+print("You are studying",input("What are you studying?> "))
