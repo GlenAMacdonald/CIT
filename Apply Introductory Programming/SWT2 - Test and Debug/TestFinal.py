3b7c83b8e7022e8f51e8ae70a153f041025eafe3
Spares <macdonald.glen.a@gmail.com>
2023-02-10 13:18:44 +1100

    update

--- Apply Introductory Programming/SWT2 - Test and Debug/TestFinal.py
+++ Apply Introductory Programming/SWT2 - Test and Debug/TestFinal.py
@@ -1,13 +1,14 @@
 ## Calculates the GPA of each student.
 ## Author: M. Ployee
-## Last Modified: 25/8/21
+## Last Modified: 26/8/2022 - by Glen Macdonald
 ## Contributors:
 ### M. Ployee
+### G. Macdonald
 
 
 #Blank variable to store user input and GPA
-totals = set()
-letters = set()
+totals = list()
+letters = list()
 
 #Get and store user input
 print ("Please enter the grade for each student, or press q to quit")
@@ -15,20 +16,20 @@ while(True):
     user_input = input("> ")
     if(user_input.lower() == "q"):
         break
-    totals.add(user_input)
+    totals.append(int(user_input))
 
 #Caclulate letter grade for each user response
 for total in totals:
-    if total <= "59":
-        letters.add("F")
-    elif total <= "69":
-        letters.add("P")
-    elif total <= "79":
-        letters.add("C")
-    elif total <= "89":
-        letters.add("D")
+    if total <= 59:
+        letters.append("F")
+    elif total <= 69:
+        letters.append("P")
+    elif total <= 79:
+        letters.append("C")
+    elif total <= 89:
+        letters.append("D")
     else:
-        letters.add("HD")
+        letters.append("HD")
 
 #Print GPA results.
 print("The GPA for your students is: {}".format(totals))
